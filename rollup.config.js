9fbea723edb9f274cbd55ed6f6e750ffd69f4e0d
Jason Sturges <jason@jasonsturges.com>
2021-08-30 22:42:06 -0500

    Refactoring

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,31 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json";
 
 export default [
   // browser-friendly UMD build
   {
-    input: "src/index.ts",
+    input: "src/index.js",
     output: {
-      name: "typescriptNpmPackage",
+      name: "PixiEditor",
       file: pkg.browser,
       format: "umd",
       globals: { "pixi.js": "PIXI" },
     },
-    external: ["pixi.js"],
+    external: [ "pixi.js" ],
     plugins: [
       resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      commonjs()
     ],
   },
 
   // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
-    input: "src/index.ts",
+    input: "src/index.js",
     output: [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" },
     ],
-    external: ["pixi.js"],
-    plugins: [typescript({ tsconfig: "./tsconfig.json" })],
+    external: [ "pixi.js" ],
   },
 ];
